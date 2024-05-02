3b32dbcc20b421fd7f2f36d1256ec7bd90193325
g-beier <gustavbeier@gmail.com>
2024-05-02 12:08:26 -0300

    Added Header

--- container/src/App.js
+++ container/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
 import MarketingApp from "./components/MarketingApp";
 
 export default () => {
   return (
-    <>
-      <h1>Container app</h1>
+    <BrowserRouter>
+      <Header />
       <MarketingApp />;
-    </>
+    </BrowserRouter>
   );
 };
